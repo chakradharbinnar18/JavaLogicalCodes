cf28ca7b814edd482f19d9dac983d11fb4311d83
chakradharbinnar18 <chakradharbinnar18@gmail.com>
2026-09-24 14:55:05 +0530

    Update AverageOfList.java

--- JavaCodes/src/com/Java8/AverageOfList.java
+++ JavaCodes/src/com/Java8/AverageOfList.java
@@ -7,6 +7,13 @@ public class AverageOfList {
 	public static void main(String[] args) {
 		List<Integer> list = Arrays.asList(10, 20, 30, 40);
 		double avg = list.stream().mapToInt(i->i).average().getAsDouble();
+
+//Or 
+
+double avg = list.stream()
+                .mapToInt(i -> i)
+                .average()
+                .orElse(0.0);
 		
 		System.out.println(avg);
 	}
